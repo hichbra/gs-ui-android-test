009d5cffbbd06a6c279c7f088bc4907ebe4001e0
hichbra <hicham.brahimi@laposte.net>
2018-01-10 17:36:44 +0100

    First Commit

---
+++ app/src/main/java/ui/graphstream/org/gs_ui_androidtest/Activity_ColorInterpolation.java
@@ -0,0 +1,158 @@
+package ui.graphstream.org.gs_ui_androidtest;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.FrameLayout;
+
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.MultiGraph;
+import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.ui.android.viewer.util.DefaultFragment;
+import org.graphstream.ui.view.Viewer;
+import org.graphstream.ui.view.ViewerListener;
+import org.graphstream.ui.view.ViewerPipe;
+
+public class Activity_ColorInterpolation extends AppCompatActivity implements ViewerListener {
+
+    private static final int CONTENT_VIEW_ID = 10101010;
+    private DefaultFragment fragment ;
+    private Graph graph ;
+    protected boolean loop = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FrameLayout frame = new FrameLayout(this);
+        frame.setId(CONTENT_VIEW_ID);
+        setContentView(frame, new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT));
+
+        graph = new MultiGraph("Color Interpolation");
+        graph.setAttribute("ui.antialias");
+
+        display(savedInstanceState, graph, true);
+    }
+
+    /**
+     * In onStart, the AndroidViewer is already created
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        ViewerPipe pipe = fragment.getViewer().newViewerPipe();
+        pipe.addViewerListener(this);
+
+        Node A = graph.addNode("A");
+        Node B = graph.addNode("B");
+        Node C = graph.addNode("C");
+
+        graph.addEdge("AB", "A", "B", true);
+        graph.addEdge("BC", "B", "C", true);
+        graph.addEdge("CA", "C", "A", true);
+
+        A.setAttribute("xyz", 0, 1, 0);
+        B.setAttribute("xyz", 1, 0, 0);
+        C.setAttribute("xyz", -1, 0, 0);
+
+        graph.setAttribute("ui.stylesheet", styleSheet);
+
+        new Thread( () ->  {
+            float color = 0;
+            float dir = 0.01f;
+
+              while (loop) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                pipe.pump();
+
+                color += dir;
+
+                if (color > 1) {
+                    color = 1;
+                    dir = -dir;
+                } else if (color < 0) {
+                    color = 0;
+                    dir = -dir;
+                }
+
+                A.setAttribute("ui.color", color);
+                showSelection(graph);
+            }
+
+            System.exit(0);
+        }).start();
+    }
+
+    public void display(Bundle savedInstanceState, Graph graph, boolean autoLayout) {
+        if (savedInstanceState == null) {
+            FragmentManager fm = getFragmentManager();
+
+            // find fragment or create him
+            fragment = (DefaultFragment) fm.findFragmentByTag("fragment_tag");
+            if (null == fragment) {
+                DefaultFragment.autoLayout = autoLayout ;
+                fragment = new DefaultFragment();
+                fragment.init(graph);
+            }
+
+            // Add the fragment in the layout and commit
+            FragmentTransaction ft = fm.beginTransaction() ;
+            ft.add(CONTENT_VIEW_ID, fragment).commit();
+        }
+    }
+
+    protected void showSelection(Graph graph) {
+        boolean selection = false;
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("[");
+
+        for (Node node : graph) {
+            if (node.hasAttribute("ui.selected")) {
+                sb.append(String.format(" %s", node.getId()));
+                selection = true;
+            }
+            if (node.hasAttribute("ui.clicked")) {
+                Log.e("Debug", "node "+node.getId()+" clicked ");
+            }
+        }
+
+        sb.append(" ]");
+
+        if (selection)
+            Log.e("Debug", "selection = "+sb.toString());
+    }
+
+    protected static String styleSheet = "graph { padding: 20px; stroke-width: 0px; }"
+            + "node {size: 48,48;}"
+            + "node:selected { fill-color: red;  fill-mode: plain; }"
+            + "node:clicked  { fill-color: blue; fill-mode: plain; }"
+            + "node#A        { fill-color: green, yellow, purple; fill-mode: dyn-plain; }"
+            + "edge{ size: 5;}";
+
+    public void buttonPushed(String id) {
+    }
+
+    public void buttonReleased(String id) {
+    }
+
+    public void mouseOver(String id) {
+    }
+
+    public void mouseLeft(String id) {
+    }
+
+    public void viewClosed(String viewName) {
+        loop = false;
+    }
+}
---
+++ app/src/main/java/ui/graphstream/org/gs_ui_androidtest/Activity_withoutXML.java
@@ -0,0 +1,96 @@
+package ui.graphstream.org.gs_ui_androidtest;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.ui.android.viewer.util.DefaultFragment;
+
+public class Activity_withoutXML extends AppCompatActivity {
+
+    private static final int CONTENT_VIEW_ID = 10101010;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FrameLayout frame = new FrameLayout(this);
+        frame.setId(CONTENT_VIEW_ID);
+        setContentView(frame, new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT));
+
+        Graph graph = new SingleGraph("Tutorial 1");
+
+        graph.setAttribute("ui.stylesheet", "" +
+                "graph { padding: 0px; fill-color: #EEE; }"+
+                "edge { text-size: 48; size: 5;}"+
+                "node { text-size: 48; fill-color: white; size: 40px, 40px; padding: 15px, 15px; stroke-mode: plain; stroke-color: #555; shape: box; }"
+                + "node#B {shape: circle;}"
+        );
+
+        graph.setAttribute("ui.antialias");
+
+        graph.setStrict(false);
+        graph.setAutoCreate( true );
+        Edge e = graph.addEdge( "AB", "B", "A", true);
+        e.setAttribute("weight", 5.0);
+        e = graph.addEdge( "BC", "B", "C", true);
+
+        Node b = graph.getNode("B");
+        b.setAttribute("label", "B");
+
+        e.setAttribute("weight", 1.0);
+        e = graph.addEdge( "CA", "C", "A", true);
+        e.setAttribute("weight", 2.0);
+        e = graph.addEdge( "BF", "F", "B", true);
+        e.setAttribute("weight", 1.0);
+        e = graph.addEdge( "CE", "E", "C", true);
+        e.setAttribute("weight", 8.0);
+        e = graph.addEdge( "EF", "E", "F", true);
+        e.setAttribute("weight", 1.0);
+
+        for(int i = 0 ; i < graph.getNodeCount() ; i++){
+            graph.getNode(i).setAttribute("ui.label", graph.getNode(i).getId());
+        }
+
+        for(int i = 0 ; i < graph.getEdgeCount() ; i++){
+            graph.getEdge(i).setAttribute("ui.label", "Poids = "+(graph.getEdge(i).getAttribute("weight")));
+        }
+
+        Log.e("Debug","BEFORE");
+
+        display(savedInstanceState, graph, true);
+
+        Log.e("Debug", "AFTER");
+    }
+
+    public void display(Bundle savedInstanceState, Graph graph, boolean autoLayout) {
+        //Cependant, si le fragment est restauré à partir d'un état précédent, alors il n'y a pas besoin d'effectuer quoi que ce soit et il faudrait juste
+        // effectuer le return, sinon nous pourrions nous retrouver avec des fragments qui se chevauchent.
+        if (savedInstanceState == null) {
+            // Récupérez le FragmentManager
+            FragmentManager fm = getFragmentManager();
+            // Trouver si le fragment que nous souhaitons afficher appartient à la backstack
+            DefaultFragment fragment = (DefaultFragment) fm.findFragmentByTag("fragment_tag");
+
+            if (null == fragment) {
+                // Créez le fragment
+                DefaultFragment.autoLayout = autoLayout ;
+                fragment = new DefaultFragment();
+                fragment.init(graph);
+            }
+            //Dans notre cas, l'activité a été démarrée avec des paramètres spécifiques contenus dans l'Intent. Passez ces paramètres à votre fragment
+            //fragment.setArguments(getIntent().getExtras());
+            // Ajoutez le fragment à son layout et effectuez le commit
+            FragmentTransaction ft = fm.beginTransaction() ;
+            ft.add(CONTENT_VIEW_ID, fragment).commit();
+        }
+    }
+}
